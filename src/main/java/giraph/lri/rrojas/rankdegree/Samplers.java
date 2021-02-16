186fd1c83a945c256d321774d6bd7510dae762e2
JulioCandela1993 <julio.christians.candela@est.fib.upc.edu>
2021-02-16 20:17:31 +0100

    Code

--- src/main/java/giraph/lri/rrojas/rankdegree/Samplers.java
+++ src/main/java/giraph/lri/rrojas/rankdegree/Samplers.java
@@ -671,6 +671,10 @@ public class Samplers extends LPGPartitionner {
 					int triangles = 0;
 					for (SamplingMessage msg : messages) {
 						LongArrayListWritable tmp = (LongArrayListWritable)msg.getFriendlist();
+						for(IntWritable id: tmp){
+							System.out.println("Holi");
+						}
+
 						/*for (Object id : tmp) {
 							if (friends.contains((IntWritable)id)) {
 								// Triangle found
