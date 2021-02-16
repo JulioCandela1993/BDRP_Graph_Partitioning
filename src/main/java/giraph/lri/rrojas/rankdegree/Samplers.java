1e07fca4495240ca1047da134af24c24e47cff68
JulioCandela1993 <julio.christians.candela@est.fib.upc.edu>
2021-02-16 19:11:03 +0100

    Code

--- src/main/java/giraph/lri/rrojas/rankdegree/Samplers.java
+++ src/main/java/giraph/lri/rrojas/rankdegree/Samplers.java
@@ -540,9 +540,9 @@ public class Samplers extends LPGPartitionner {
 				}
 
 				Collections.sort(values, Collections.reverseOrder());
-				/*
-				sigma_vertex = (Integer)totalVertexNumber*SIGMA;
 
+				sigma_vertex = (int)(totalVertexNumber*SIGMA);
+/*
 				minCC = Collections.min(values.subList(0,sigma_vertex));
 				*/
 
